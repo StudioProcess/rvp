42fa0a33b0f482b8f5493b9a70abd5e2ff46daa7
Martin Leopold <m@martinleopold.com>
2016-05-09 17:43:02 +0200

    flowplayer added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -97,7 +97,8 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
     server: {
       baseDir: ['.tmp', 'app'],
       routes: {
-        '/bower_components': 'bower_components'
+        '/bower_components': 'bower_components',
+        '/node_modules': 'node_modules'
       }
     },
     browser: "google chrome"
