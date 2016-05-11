3691a5434a1dc7885401689fc7132be813548353
RESL <moritz@resl.xyz>
2016-05-11 17:19:23 +0200

    started annotationBarCtrl

---
+++ app/scripts/controllers/annotationBarCtrl.js
@@ -0,0 +1,9 @@
+angular.module('rvApp')
+  .controller('annotationBarCtrl', function ($log) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $log.log('Controller: annotationBarCtrl');
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,3 +16,13 @@ angular.module('rvApp')
     ];
     $log.log('Controller: MainCtrl');
   });
+
+  angular.module('rvApp')
+    .controller('annotationBarCtrl', function ($log) {
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+      $log.log('Controller: annotationBarCtrl');
+    });
