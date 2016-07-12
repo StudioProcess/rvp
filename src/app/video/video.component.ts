2bc9032f080de350344e8974f4a0a8b4c79e158f
Martin Leopold <m@martinleopold.com>
2016-07-12 18:45:40 +0200

    Variable rate polling of current player time

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, AfterViewInit, Input, ElementRef } from '@angular/core';
 import 'video.js'; // creates global videojs() function
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from "rxjs/Subject";
+// import 'rxjs/observable/interval';
+import 'rxjs/Rx'; // TODO: proper feature imports for Rx
+
+
 @Component({
   moduleId: module.id,
   selector: 'app-video',
@@ -12,6 +18,12 @@ export class VideoComponent implements OnInit, AfterViewInit {
   player; // VideoJS Player instance
   playerReady; // Promise that resolves when player is ready
   playerOptions = {}; // Options to pass to VideoJS player
+
+  private timepolling:Observable<number>; //
+  private pollingSubscription;
+
+  timeupdate:Subject<number> = new Subject<number>();
+  timeupdateRate = 30; // ms
 
   @Input()
   set videoSrc(src) {
@@ -33,6 +45,14 @@ export class VideoComponent implements OnInit, AfterViewInit {
       resolveFn = resolve;
     });
     this.playerReady.resolve = resolveFn;
+
+    this.timepolling = Observable.interval(this.timeupdateRate).map(() => {
+      return this.player.currentTime();
+    });
+
+    this.timeupdate.subscribe((time) => {
+      log.debug('polling time', time);
+    });
   }
 
   ngOnInit() {
@@ -49,22 +69,26 @@ export class VideoComponent implements OnInit, AfterViewInit {
     });
   }
 
-  fitPlayer() {
+  private fitPlayer() {
     let elementDim = this.hostElement.nativeElement.getBoundingClientRect();
     this.player.dimensions(elementDim.width, elementDim.height);
   }
 
   onPlay(event?) {
     log.debug('play', event);
+    this.pollingSubscription = this.timepolling.subscribe(this.timeupdate);
   }
 
   onPause(event?) {
     log.debug('pause', event);
+    this.pollingSubscription.unsubscribe();
   }
 
   onSeeking(event?) {
     log.debug('seeking', event);
+    this.timeupdate.next(this.player.currentTime());
   }
+
 
   /*
     Video.js Player API: http://docs.videojs.com/docs/api/player.html
