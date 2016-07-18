186fb330b2c50c196ad11158c644d67df478472b
Martin Leopold <m@martinleopold.com>
2016-07-18 14:37:08 +0200

    Added Scroll & Zoom directive

--- src/app/timeline/scroll-zoom.directive.ts
+++ src/app/timeline/scroll-zoom.directive.ts
@@ -1,38 +1,48 @@
-import { Directive, Input, ElementRef, OnInit, AfterViewInit } from '@angular/core';
+import { Directive, Input, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[scroll], [zoom]'
+  selector: '[zoom], [scroll]'
 })
-export class ScrollZoom implements OnInit, AfterViewInit {
+export class ScrollZoom implements OnInit {
 
-  // 0..100 (%)
-  // @Input() scroll:number) {
-  //
-  // }
+  // set width of content element: [px]
+  // content element is the first child
+  @Input() set zoom(value:number) {
+    log.debug('setting zoom', value);
+    if (value > 0) {
+      this._zoom = value;
+      if (this.content) {
+        this.content.style.width = this._zoom + "px";
+      }
+    }
+  }
 
-  // width of content
-  @Input() zoom:number = 1000;
+  // set scroll position of container: 0..100 [%]
+  // TODO: need to take into account the width of the container
+  @Input() set scroll(value:number) {
+    log.debug('setting scroll', value);
+    if (value >= 0 && value <= 100) {
+      this._scroll = value;
+      this.container.scrollLeft = this._zoom * this._scroll / 100;
+    }
+  }
 
-  @Input() scroll:number = 0;
-
+  private _zoom;
+  private _scroll;
   private container:HTMLElement;
   private content:HTMLElement;
 
   constructor(hostElement:ElementRef) {
     this.container = hostElement.nativeElement;
     this.container.style.overflow = 'hidden';
-    this.container.scrollLeft = this.scroll;
-    log.debug(this.container);
   }
 
   ngOnInit() {
-     this.content = this.container.firstElementChild as HTMLElement;
-     this.content.style.width = this.zoom + "px";
-    //  log.debug(this.content, this.zoom);
+    this.content = this.container.firstElementChild as HTMLElement;
+    setTimeout(() => {
+      // trigger setting zoom and scroll, when this.content is available
+      this.zoom = this._zoom;
+      this.scroll = this._scroll;
+    }, 0);
   }
-
-  ngAfterViewInit() {
-
-  }
-
 }
