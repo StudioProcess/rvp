9606d237e689612dda000eed818a52308e2d4fd8
Martin Leopold <m@martinleopold.com>
2016-07-18 20:42:28 +0200

    Added another drag observable that emits relative drag movements

--- src/app/timeline/handlebar/handlebar.component.ts
+++ src/app/timeline/handlebar/handlebar.component.ts
@@ -6,6 +6,12 @@ interface DragEvent {
   event:MouseEvent;
 }
 
+interface CustomDragEvent {
+  type:'dragstart'|'drag'|'dragend';
+  dx:number; // horizontal displacement
+  dy:number; // vertival displacement
+}
+
 @Component({
   moduleId: module.id,
   selector: 'app-handlebar',
@@ -31,6 +37,8 @@ export class HandlebarComponent implements OnInit {
   host:HTMLElement;
   container:HTMLElement;
 
+  private customCenterDrag:Observable<CustomDragEvent>;
+
   constructor(hostElement:ElementRef) {
     this.host = hostElement.nativeElement;
   }
@@ -40,6 +48,11 @@ export class HandlebarComponent implements OnInit {
     this.centerDrag = this.dragStream('.handlebar');
     this.leftDrag = this.dragStream('.left-handle');
     this.rightDrag = this.dragStream('.right-handle');
+
+
+    this.customCenterDrag = this.customDragStream('.handlebar');
+    this.customCenterDrag.subscribe(x => {log.debug(x)});
+
 
     // check if length shorter than minimum width
     if((this.width <= this.minWidth) && (this.width != 0)) {
@@ -88,4 +101,26 @@ export class HandlebarComponent implements OnInit {
     return drag.merge(start, end);
   }
 
+  private customDragStream(selector):Observable<CustomDragEvent> {
+    // stops propagation on the given MouseEvent
+    const stopPropagation = (e:MouseEvent) => { e.stopPropagation() };
+    // dom event streams
+    const el = this.host.querySelector(selector); // dom element with the given selector
+    const mousedown$ = Observable.fromEvent(el, 'mousedown').do(stopPropagation);
+    const mousemove$ = Observable.fromEvent(document, 'mousemove').do(stopPropagation);
+    const mouseup$ = Observable.fromEvent(document, 'mouseup').do(stopPropagation);
+
+    return mousedown$.flatMap((e:MouseEvent) => {
+      let startX = e.screenX;
+      let startY = e.screenY;
+      let start = Observable.of({type:'dragstart', dx:0, dy:0});
+      let drag = mousemove$.map((e:MouseEvent) => {
+        return {type:'drag', dx:e.screenX-startX, dy:e.screenY-startY};
+      }).takeUntil(mouseup$);
+      let end = mouseup$.first().map((e:MouseEvent) => {
+        return {type:'dragend', dx:e.screenX-startX, dy:e.screenY-startY};
+      });
+      return start.merge(drag).merge(end) as Observable<CustomDragEvent>;
+    });
+  }
 }
