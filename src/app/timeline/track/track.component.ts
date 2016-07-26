4720b45bb5044e364eeb90921d2678dbafb95f29
RESL <moritz@resl.xyz>
2016-07-26 16:21:47 +0200

    dispatch function added for new annotations in timeline/track

--- src/app/timeline/track/track.component.ts
+++ src/app/timeline/track/track.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AnnotationComponent } from './annotation';
 import { Track, Annotation } from '../../shared/models';
+import { Store } from '@ngrx/store';
 
 @Component({
   moduleId: module.id,
@@ -13,14 +14,24 @@ export class TrackComponent implements OnInit {
 
   @Input() data:Track;
 
-  constructor() {
+  constructor(private store:Store<any>) {
   }
 
   ngOnInit() {
   }
 
+  // add annotation
   doubleClick($clickEvent){
-    log.debug("double clicked");
+    let newAnnotation:Annotation = {
+      utc_timestamp: 12,  // todo: calculate accurate time corresponding to relative position on track
+      duration: 2,       // todo: calculate accurate duration corresponding to relative position on track
+      fields: {
+        title: '',
+        description: ''
+      }
+    };
+    this.store.dispatch( { type: 'ADD_ANNOTATION', payload: newAnnotation } );
+    log.debug("new annotation dispatched");
   }
 
 }
