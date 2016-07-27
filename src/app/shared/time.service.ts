b8df8143dd8856baabca96641df0d5657ec63279
Martin Leopold <m@martinleopold.com>
2016-07-27 19:25:23 +0200

    Guard against null values

--- src/app/shared/time.service.ts
+++ src/app/shared/time.service.ts
@@ -28,12 +28,12 @@ export class TimeService {
   zoomLevelStream: Observable<number>; // can also change on viewport resize
 
   constructor() {
-    this._timelineDuration = new BehaviorSubject<number>(1);
-    this._timelineViewportWidth = new BehaviorSubject<number>(1);
-    this._timelineWidth = new BehaviorSubject<number>(1);
-    this._zoomLevel = new BehaviorSubject<number>(1);
+    this._timelineDuration = new BehaviorSubject(null);
+    this._timelineViewportWidth = new BehaviorSubject(null);
+    this._timelineWidth = new BehaviorSubject(null);
+    this._zoomLevel = new BehaviorSubject(null);
 
-    this._minZoomLevel = 0.1;
+    this._minZoomLevel = null;
     this._maxZoomLevel = 100;
 
     this.timelineDurationStream = this._timelineDuration.asObservable();
@@ -42,6 +42,19 @@ export class TimeService {
     this.zoomLevelStream = this._zoomLevel.asObservable();
   }
 
+  // // utility to resend the current value of a BehaviorSubject
+  // private resend<T>(sub: BehaviorSubject<T>) {
+  //   if (sub.value != null) { sub.next(sub.value); }
+  // }
+  //
+  // //
+  // flushStreams() {
+  //   this.resend(this._timelineDuration);
+  //   this.resend(this._timelineViewportWidth);
+  //   this.resend(this._timelineWidth);
+  //   this.resend(this._zoomLevel);
+  // }
+
   // Set timeline duration [s] (> 0)
   // => Broadcasts:
   //    New timeline duration (timelineDuration)
@@ -49,6 +62,7 @@ export class TimeService {
   set timelineDuration(duration: number) {
     if (duration <= 0) throw new Error('Invalid timeline duration. (Needs to be > 0)');
     this._timelineDuration.next(duration);
+    if (!this._zoomLevel.value) return;
     this._timelineWidth.next(duration * this._zoomLevel.value);
   }
 
@@ -62,9 +76,10 @@ export class TimeService {
   //    New timeline width [px] (timelineWidth)
   set zoomLevel(zoom: number) {
     if (zoom <= 0) throw new Error('Invalid zoom level. (Needs to be > 0)');
-    if (zoom < this._minZoomLevel) { zoom = this._minZoomLevel; }
-    else if (zoom > this._maxZoomLevel) { zoom = this._maxZoomLevel; }
+    if (this._minZoomLevel && zoom < this._minZoomLevel) { zoom = this._minZoomLevel; }
+    else if (this._maxZoomLevel && zoom > this._maxZoomLevel) { zoom = this._maxZoomLevel; }
     this._zoomLevel.next(zoom);
+    if (!this._timelineDuration.value) return;
     this._timelineWidth.next(zoom * this._timelineDuration.value);
   }
 
@@ -81,10 +96,14 @@ export class TimeService {
     if (width <= 0) throw new Error('Invalid viewport width. (Needs to be > 0)');
     this._timelineViewportWidth.next(width);
 
-    // Interpolate new zoom level
+    // Adjust min zoom level to new width
+    if (!this._minZoomLevel || !this._timelineDuration) return;
     let minZoomOld = this._minZoomLevel;
     this._minZoomLevel = width / this._timelineDuration.value;
     let minZoomNew = this._minZoomLevel;
+
+    // Interpolate new zoom level
+    if (!this._zoomLevel.value) return;
     let zoomOld = this._zoomLevel.value;
     let maxZoom = this._maxZoomLevel;
     let scale = (zoomOld-minZoomOld) / (maxZoom - minZoomOld);
