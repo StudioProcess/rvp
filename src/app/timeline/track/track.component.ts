78dc5961a15acccca3a447f79c051424f16e5686
Martin Leopold <m@martinleopold.com>
2016-07-29 11:56:29 +0200

    Add annotation on track doubleclick

--- src/app/timeline/track/track.component.ts
+++ src/app/timeline/track/track.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AnnotationComponent } from './annotation';
 import { Track, Annotation } from '../../shared/models';
 import { Store } from '@ngrx/store';
+import { TimeService } from '../../shared/time.service';
 
 @Component({
   moduleId: module.id,
@@ -14,26 +15,26 @@ export class TrackComponent implements OnInit {
 
   @Input() data:Track;
 
-  constructor(private store:Store<any>) {
+  constructor(private store:Store<any>, private timeService:TimeService) {
   }
 
   ngOnInit() {
   }
 
   // add annotation
-  doubleClick($clickEvent){
+  doubleClick(e) {
+    let clickPosition = e.offsetX + e.target.scrollLeft;
+    let clickTime = this.timeService.convertPixelsToSeconds(clickPosition);
+
     let newAnnotation:Annotation = {
-      utc_timestamp: 12,  // todo: calculate accurate time corresponding to relative position on track
-      duration: 2,       // todo: calculate accurate duration corresponding to relative position on track
+      utc_timestamp: clickTime,  // todo: calculate accurate time corresponding to relative position on track
+      duration: 0,
       fields: {
         title: '',
         description: ''
       },
       isSelected: false
     };
-    this.store.dispatch( { type: 'ADD_ANNOTATION', payload: { annotation: newAnnotation, track: this.data }} );
-    //     this.cursorTime = $moveEvent.offsetX / $moveEvent.currentTarget.offsetWidth * 100,100;
-    log.debug($clickEvent.offsetX);// - $('timeline').offset().left);
+    this.store.dispatch( { type: 'ADD_ANNOTATION', payload: { annotation: newAnnotation, track: this.data } } );
   }
-
 }
