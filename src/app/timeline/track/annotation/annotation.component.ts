156a5c5a84db60e5d119cedad571a3f14ef90849
Martin Leopold <m@martinleopold.com>
2016-07-29 13:10:57 +0200

    Update annotations on dragend

--- src/app/timeline/track/annotation/annotation.component.ts
+++ src/app/timeline/track/annotation/annotation.component.ts
@@ -3,6 +3,7 @@ import { TimelineComponent } from '../../timeline.component';
 import { Annotation, Timeline } from '../../../shared/models';
 import { HandlebarComponent } from '../../handlebar/handlebar.component';
 import { Store } from '@ngrx/store';
+import { TimeService } from '../../../shared/time.service';
 
 @Component({
   moduleId: module.id,
@@ -22,8 +23,11 @@ export class AnnotationComponent implements OnInit {
   timestamp:number;
   duration:number;
 
-  constructor(@Inject(forwardRef(() => TimelineComponent)) private timeline, private store:Store<any> ) {
-  }
+  constructor(
+    @Inject(forwardRef(() => TimelineComponent)) private timeline,
+    private store:Store<any>,
+    private timeService:TimeService
+  ) {}
 
   ngOnInit() {
     this.timestamp = this.data.utc_timestamp / this.timeline.data.duration * 100;
@@ -33,6 +37,17 @@ export class AnnotationComponent implements OnInit {
       this.duration = this.data.duration / this.timeline.data.duration * 100;
     }
     // log.debug('timeline duration', this.timeline.data.duration, 'annotation', this.timestamp, this.duration);
+
+    // Update this annotation on dragend
+    this.handlebar.drag.filter(e => e.type == 'dragend').subscribe(e => {
+      // log.debug(e);
+      let newAnnotation = Object.assign({}, this.data, {
+        utc_timestamp: this.timeService.convertPercentToSeconds(e.left / 100),
+        duration: this.timeService.convertPercentToSeconds(e.width / 100)
+      });
+      // log.debug(newAnnotation);
+      this.store.dispatch({ type: 'UPDATE_ANNOTATION', payload: { old: this.data, new: newAnnotation } });
+    });
   }
 
   clickAnnotation($clickEvent){
