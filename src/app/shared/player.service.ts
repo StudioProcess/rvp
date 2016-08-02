a5a04adf65850a5ea20b6c2f1eda2623f3f04a75
Martin Leopold <m@martinleopold.com>
2016-08-02 16:54:18 +0200

    Added back channel (Player -> App) and 'videoLoaded' event

--- src/app/shared/player.service.ts
+++ src/app/shared/player.service.ts
@@ -7,17 +7,38 @@ interface PlayerAction {
   [key:string]: any;
 }
 
+interface PlayerEvent {
+  type: 'videoLoaded';
+  [key:string]: any;
+}
+
+interface VideoMetadata {
+  src: string;
+  type: string;
+  duration: number;
+  width: number;
+  height: number;
+  file: {
+    [key:string]: any;
+  }
+}
+
 @Injectable()
 export class PlayerService {
 
   private _action: Subject<PlayerAction>;
-  actionStream: Observable<PlayerAction>;
+  private _event: Subject<PlayerEvent>;
+  actionStream: Observable<PlayerAction>; // Actions (App    -> Player)
+  eventStream: Observable<PlayerEvent>;   // Events  (Player ->    App)
 
   constructor(private timeService: TimeService) {
     this._action = new Subject<PlayerAction>();
+    this._event = new Subject<PlayerEvent>();
     this.actionStream = this._action.asObservable();
+    this.eventStream = this._event.asObservable();
   }
 
+  // Actions (App -> Player)
   setTime(s: number) {
     this._action.next( {type:'setTime', time:s} );
   }
@@ -37,6 +58,11 @@ export class PlayerService {
 
   toggle() {
     this._action.next( {type:'toggle'} );
+  }
+
+  // Events (Player -> App)
+  videoLoaded(meta: VideoMetadata) {
+    this._event.next( {type:'videoLoaded', meta} );
   }
 
 }
