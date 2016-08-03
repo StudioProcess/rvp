fb49a6a67ee5ff4b920b4236c6b342630bc6010e
Martin Leopold <m@martinleopold.com>
2016-08-03 11:51:40 +0200

    Added project import method

--- src/app/backend/project-io.service.ts
+++ src/app/backend/project-io.service.ts
@@ -8,22 +8,35 @@ import * as JSZip from 'jszip';
 export class ProjectIOService {
 
   constructor() {
-    log.debug('filesaver', saveAs);
-    log.debug('jszip', JSZip);
+    // log.debug('filesaver', saveAs);
+    // log.debug('jszip', JSZip);
   }
 
-  export(data: Project, videoBlob: Blob, filename?: string) {
+  export(data: Project, videoBlob: Blob, filename?: string): Promise<any> {
     let zip = new JSZip();
     let filesaver:any = saveAs;
     zip.file('project.json', JSON.stringify(data));
     // zip.file('video.mp4', videoBlob, {binary: true});
-    zip.generateAsync({type: 'blob'}).then( blob => {
-      filesaver(blob, filename || 'project.rvp');
+    return zip.generateAsync({type: 'blob'}).then( blob => {
+      filesaver(blob, filename || 'project.rv');
     });
   }
 
-  import(filename): Project {
-    return null;
+  import(zipfile: File): Promise<any> {
+    let zip = new JSZip();
+    return zip.loadAsync(zipfile).then(zip => {
+
+      let dataPromise = zip.file('project.json').async('string').then(json => {
+        return JSON.parse(json);
+      });
+      let videoBlobPromise = zip.file('video.mp4').async('uint8array').then(uint8array => {
+        return new Blob([uint8array]);
+      });
+      return Promise.all([dataPromise, videoBlobPromise]).then( ([data, videoBlob]) => {
+        return {data, videoBlob};
+      });
+
+    });
   }
 
 }
