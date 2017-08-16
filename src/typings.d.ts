2f1b95842eb8419540c301edd0f82ed6a99931d3
Martin Leopold <m@martinleopold.com>
2017-08-16 16:36:56 +0200

    Updated to latest foundation (6.4.2)

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,7 @@
+/* foundation types */
+/* included here since foundation is used as global script in .angular-cli.json */
+/// <reference path="../node_modules/foundation-sites/dist/js/foundation.d.ts" />
+
 /* SystemJS module definition */
 declare var module: NodeModule;
 interface NodeModule {
