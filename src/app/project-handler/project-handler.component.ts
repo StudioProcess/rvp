157472c9dc809e0cc18e83ca9693c590a5046114
Onur D <contact@dinony.com>
2017-09-02 19:37:19 +0200

    Cleanup imports

--- src/app/project-handler/project-handler.component.ts
+++ src/app/project-handler/project-handler.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FilepickerComponent } from './filepicker';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-project-handler',
