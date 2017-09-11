5c4bb41c802cb10a2a2c7c9e092097790f9a12ba
Onur D <contact@dinony.com>
2017-09-11 14:20:38 +0200

    Prep reducers

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,26 @@
+import {ActionReducerMap, createSelector, createFeatureSelector} from '@ngrx/store'
+
+import * as fromProject from '../core/reducers/project'
+import * as fromLoading from '../core/reducers/loading'
+
+export interface State {
+  project: fromProject.State,
+  loading: fromLoading.State
+}
+
+export const reducers: ActionReducerMap<State> = {
+  project: fromProject.reducer,
+  loading: fromLoading.reducer
+}
+
+export const getProjectState = createFeatureSelector<fromProject.State>('project')
+export getVideo = createSelector(
+  getProjectState,
+  get
+)
+
+export const getLoadingState = createFeatureSelector<fromLoading.State>('loading')
+
+export const getIsLoading = createSelector(
+  getLoadingState,
+  fromLoading.getIsAppLoading)
