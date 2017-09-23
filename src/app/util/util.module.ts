78ecf390b9fad435779be255df7d831f7bdd1954
Onur D <contact@dinony.com>
2017-09-23 17:54:37 +0200

    Prep binary data loader and util module

---
+++ src/app/util/util.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core'
+
+import {BinaryDataLoader} from './BinaryDataLoader'
+
+const _DECLS_ = [BinaryDataLoader]
+
+@NgModule({
+  exports: _DECLS_
+})
+export class UtilModule {}
