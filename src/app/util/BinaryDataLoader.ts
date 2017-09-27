747a02e79c984c58fb7b5d670c800039db0788a2
Onur D <contact@dinony.com>
2017-09-27 17:21:45 +0200

    Prep ProjectLoader

--- src/app/util/BinaryDataLoader.ts
+++ src/app/util/BinaryDataLoader.ts
@@ -3,20 +3,15 @@ import {Injectable} from '@angular/core'
 import {getBinaryContent} from 'jszip-utils'
 
 import {Observable} from 'rxjs/Observable'
-import 'rxjs/add/observable/bindCallback'
+import 'rxjs/add/observable/bindNodeCallback'
 import 'rxjs/add/observable/throw'
 import 'rxjs/add/observable/of'
 
 @Injectable()
 export class BinaryDataLoader {
-  private readonly _getBinaryFunc =
-    Observable.bindCallback(getBinaryContent, (err: Error, data: string|ArrayBuffer) => {
-      return err ? Observable.throw(err) : Observable.of(data)
-    })
+  private readonly _getBinaryFunc = Observable.bindNodeCallback(getBinaryContent)
 
-  load(paths: Observable<string>) {
-    return paths.concatMap(path => {
-      return this._getBinaryFunc(path)
-    })
+  load(path: Observable<string>) {
+    return path.concatMap(p => this._getBinaryFunc(p))
   }
 }
---
+++ src/app/util/ProjectLoader.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core'
+
+import {BinaryDataLoader} from './BinaryDataLoader'
+import {ZipHandler} from './ZipHandler'
+
+import {Observable} from 'rxjs/Observable'
+
+@Injectable()
+export class ProjectLoader {
+  constructor(
+    private readonly _binLoader: BinaryDataLoader,
+    private readonly _zipHandler: ZipHandler) {}
+
+  load(path: Observable<string>) {
+    return this._zipHandler.unzip(this._binLoader.load(path))
+  }
+
+  save() {
+
+  }
+}
