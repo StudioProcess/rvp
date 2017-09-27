074ac470d2aed4470568fee811205d815c9f2821
Onur D <contact@dinony.com>
2017-09-27 17:26:47 +0200

    Adapt ServerProxy to load default project when cache is empty

---
+++ src/app/persistence/proxy/ServerProxy.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core'
+
+import {Effect, Actions} from '@ngrx/effects'
+
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/zip'
+import 'rxjs/add/operator/concatMap'
+
+import * as project from '../../core/actions/project'
+
+import {IServer} from '../server/IServer'
+import LFCache from '../cache/LFCache'
+
+import {_DEFAULTPRJPATH_} from '../../config/config'
+
+@Injectable()
+export default class ServerProxy implements IServer {
+  constructor(
+    private readonly actions: Actions,
+    private readonly _cache: LFCache,
+    private readonly _projectLoader: any) {}
+
+  @Effect()
+  fetch = this.actions
+    .ofType<project.ProjectFetch>(project.PROJECT_FETCH)
+    .concatMap(action => {
+      return Observable.zip(
+        Observable.of(action),
+        this._cache.isCached(Observable.of(action.payload.id)))
+    })
+    .concatMap(([{payload: {id}}, isCached]) => {
+      return isCached ?
+        this._cache.getCached(id).map(res => new project.ProjectFetched(res)) :
+        this._projectLoader.load(Observable.of(_DEFAULTPRJPATH_)).map(res => new project.ProjectFetched(res))
+    })
+}
