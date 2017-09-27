4d1aedddbffd46f7631c979a454a3846391167a8
Onur D <contact@dinony.com>
2017-09-27 20:51:04 +0200

    Adapt typing

--- src/app/config/project.ts
+++ src/app/config/project.ts
@@ -1,12 +1,14 @@
+import {OutputType} from 'jszip'
+
 export const _DEFAULT_PROJECT_PATH_ = 'assets/projects/default.rv'
 
 export interface ZipFileMeta {
   file: string
-  type: string
+  type: OutputType
   map: string
 }
 
 export const _ZIP_META_: ZipFileMeta[] = [
-  {file: 'default/project.json', type: 'string', map: 'description'},
+  {file: 'default/project.json', type: 'text', map: 'timeline'},
   {file: 'default/video.m4v', type: 'blob', map: 'video'}
 ]
