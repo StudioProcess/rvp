dca4de5747de19603620c1e6ebd82a9ee631e49c
Onur D <contact@dinony.com>
2017-09-28 17:25:09 +0200

    Add middleware to file metadata

--- src/app/config/project.ts
+++ src/app/config/project.ts
@@ -1,14 +1,34 @@
 import {OutputType} from 'jszip'
+import {id} from '../lib/fp'
 
 export const _DEFAULT_PROJECT_PATH_ = 'assets/projects/default.rv'
+
+export interface FileMiddleware {
+  postLoad(res: any): any
+}
+
+const defaultFileMW = {postLoad: id}
 
 export interface ZipFileMeta {
   file: string
   type: OutputType
-  map: string
+  map: string,
+  middleware: FileMiddleware
 }
 
 export const _ZIP_META_: ZipFileMeta[] = [
-  {file: 'default/project.json', type: 'text', map: 'timeline'},
-  {file: 'default/video.m4v', type: 'blob', map: 'video'}
+  {
+    file: 'default/project.json',
+    type: 'text',
+    map: 'project',
+    middleware: {
+      postLoad: res => JSON.parse(res)
+    }
+  },
+  {
+    file: 'default/video.m4v',
+    type: 'blob',
+    map: 'videoBlob',
+    middleware: defaultFileMW
+  }
 ]
