f4979b7ec3064da18d06edcfd719b9caf3569364
Onur D <contact@dinony.com>
2017-09-28 17:26:57 +0200

    Prep project reducer

--- src/app/core/reducers/project.ts
+++ src/app/core/reducers/project.ts
@@ -1,19 +1,33 @@
+import * as project from '../actions/project'
 
 export interface State {
+  id: string|null,
   video: any
   timeline: any
+  videoBlob: Blob|null
 }
 
 const initialState: State = {
+  id: null,
   video: null,
-  timeline: null
+  timeline: null,
+  videoBlob: null
 }
 
 export function reducer(state: State = initialState, action: any): State {
-  return state
+  switch(action.type) {
+    case project.PROJECT_FETCHED:
+      return {
+        ...action.payload.project,
+        videoBlob: action.payload.videoBlob
+      }
+    default:
+      return state
+  }
 }
 
 export const getVideo = (state: State) => state.video
 
-export const getTimeline = (state: State) => state.timeline
+export const getVideoSrc = (state: State) => URL.createObjectURL(state.video)
 
+export const getTimeline = (state: State) => state.timeline
