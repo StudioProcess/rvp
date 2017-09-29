5e790566a51e7249dd965e8ae1380d5e83d1a589
Onur D <contact@dinony.com>
2017-09-29 18:34:48 +0200

    Add player default options config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,3 +1,4 @@
 export * from './project'
 export * from './storage'
 export * from './zip'
+export * from './player'
---
+++ src/app/config/player.ts
@@ -0,0 +1,5 @@
+import {PlayerOptions} from 'video.js'
+
+export const _DEFAULT_PLAYER_OPTIONS_: PlayerOptions = {
+  controls: true
+}
