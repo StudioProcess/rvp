93c6182031aedd714446c6bd20ca9f7f449668fd
Onur D <contact@dinony.com>
2017-09-29 18:35:24 +0200

    Prep player container

---
+++ src/app/core/containers/player.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit, OnDestroy} from '@angular/core'
+
+@Component({
+  selector: 'rv-player'
+  template: ``
+})
+export class PlayerContainer implements OnInit, OnDestroy {
+  conststructor(private readonly _store: Store<fromPlayer.State) {}
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+}
