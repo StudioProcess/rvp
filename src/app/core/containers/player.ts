afe4e6daf2f6bab4f2d8e514a52c3ade56cec567
Onur D <contact@dinony.com>
2017-10-10 17:46:54 +0200

    Prepare player effect usage in player container

--- src/app/core/containers/player.ts
+++ src/app/core/containers/player.ts
@@ -1,17 +1,49 @@
-import {Component, OnInit, OnDestroy} from '@angular/core'
+import {
+  Component, ViewChild, ElementRef,
+  Input, AfterViewInit, OnDestroy
+} from '@angular/core'
+
+import {Store} from '@ngrx/store'
+
+import {Observable} from 'rxjs/Observable'
+import {Subscription} from 'rxjs/Subscription'
+
+import * as fromPlayer from '../../player/reducers'
+import * as player from '../../player/actions'
+
+import {_DEFAULT_PLAYER_OPTIONS_} from '../../config'
 
 @Component({
-  selector: 'rv-player'
-  template: ``
+  selector: 'rv-player',
+  template:
+    `<div class="video-area">
+      <video #video class="video-js video-file vjs-big-play-centered"></video>
+    </div>`
 })
-export class PlayerContainer implements OnInit, OnDestroy {
-  conststructor(private readonly _store: Store<fromPlayer.State) {}
+export class PlayerContainer implements AfterViewInit, OnDestroy {
+  private readonly _subs: Subscription[] = []
 
-  ngOnInit() {
+  @ViewChild('video') private readonly _videoElem: ElementRef;
 
+  @Input() rvVideoObjectURL: Observable<string>
+
+  constructor(private readonly _store: Store<fromPlayer.State>) {}
+
+  ngAfterViewInit() {
+    this._subs.push(this.rvVideoObjectURL.subscribe(videoObjURL => {
+      const playerPayload = {
+        elemRef: this._videoElem,
+        objectURL: videoObjURL,
+        playerOptions: _DEFAULT_PLAYER_OPTIONS_
+      }
+
+      this._store.dispatch(new player.PlayerCreate(playerPayload))
+    }))
   }
 
   ngOnDestroy() {
+    this._subs.forEach(s => s.unsubscribe())
 
+    this._store.dispatch(new player.PlayerDestroy())
   }
 }
