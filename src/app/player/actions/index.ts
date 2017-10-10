fd01c01913b5390f811ef94ed1177b95ee50e056
Onur D <contact@dinony.com>
2017-10-10 16:12:57 +0200

    Prep player actions

--- src/app/player/actions/index.ts
+++ src/app/player/actions/index.ts
@@ -1,16 +1,28 @@
+import {ElementRef} from '@angular/core'
+
 import {Action} from '@ngrx/store'
 
 export const PLAYER_CREATE = '[Player] Create'
 export const PLAYER_DESTROY = '[Player] Destroy'
+export const PLAYER_CREATE_ERROR = '[Player] Create Error'
+
+interface PlayerCreatePayload {
+  elemRef: ElementRef
+  objectURL: string
+}
 
 export class PlayerCreate implements Action {
   readonly type = PLAYER_CREATE
-  constructor(readonly playload: any) {}
+  constructor(readonly playload: PlayerCreatePayload) {}
 }
 
 export class PlayerDestroy implements Action {
   readonly type = PLAYER_DESTROY
-  constructor() {}
 }
 
-export type PlayerActions = PlayerCreate|PlayerDestroy
+export class PlayerCreateError implements Action {
+  readonly type = PLAYER_CREATE_ERROR
+  constructor(readonly payload: any) {}
+}
+
+export type PlayerActions = PlayerCreate|PlayerDestroy|PlayerCreateError
