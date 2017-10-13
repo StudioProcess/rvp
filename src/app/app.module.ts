3c94ab52778a425652662de49b7052f2551928b0
Onur D <contact@dinony.com>
2017-10-13 10:24:01 +0200

    Remove comments in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,68 +5,6 @@ import {StoreModule} from '@ngrx/store'
 import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 import {EffectsModule} from '@ngrx/effects'
 
-// import {HttpModule} from '@angular/http'
-// import { ReactiveFormsModule } from '@angular/forms';
-// import { StoreModule } from '@ngrx/store';
-
-// import { reducers } from './reducers';
-// import { getEmptyData/*, getTutorialData, getMockData, getRulerData */} from './shared/datasets';
-
-// /* Pipes */
-// import { TimePipe, UnixTimePipe } from './shared';
-
-// /* Directives */
-// import { KeyDirective } from './shared';
-// import { ScrollZoom } from './timeline/';
-
-// /* Services */
-// import { InspectorService, PlayerService, PlayheadService, TimelineService } from './shared';
-// import { SimpleBackendService, ProjectIOService } from './backend';
-
-// /* Components */
-// import { AppComponent } from './app.component';
-// import { VideoComponent } from './video';
-// import { FilepickerComponent } from './project-handler/filepicker';
-// import { ProjectHandlerComponent } from './project-handler';
-// import { EntryComponent } from './inspector/entry';
-// import { InspectorComponent } from './inspector';
-// import { CursorComponent } from './timeline/cursor';
-// import { HandlebarComponent } from './timeline/handlebar';
-// import { PlayheadComponent } from './timeline/playhead';
-// import { AnnotationComponent } from './timeline/track/annotation';
-// import { TrackComponent } from './timeline/track';
-// import { TimelineComponent } from './timeline';
-
-// @NgModule({
-//   declarations: [
-//     TimePipe, UnixTimePipe,
-//     KeyDirective, ScrollZoom,
-
-//     AppComponent,
-//     VideoComponent,
-//     FilepickerComponent,
-//     ProjectHandlerComponent,
-//     EntryComponent,
-//     InspectorComponent,
-//     CursorComponent,
-//     HandlebarComponent,
-//     PlayheadComponent,
-//     AnnotationComponent,
-//     TrackComponent,
-//     TimelineComponent
-//   ],
-//   imports: [
-//     BrowserModule, HttpModule, ReactiveFormsModule,
-//     StoreModule.forRoot(reducers, {initialState: {project: getEmptyData()}})
-//   ],
-//   providers: [
-//     InspectorService, PlayerService, PlayheadService, TimelineService,
-//     SimpleBackendService, ProjectIOService
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import {environment} from '../environments/environment'
 
 import {CoreModule} from './core/core.module'
