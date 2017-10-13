ba14e52f2d45275772c9701f39d2ae83fac5d715
Onur D <contact@dinony.com>
2017-10-13 16:31:58 +0200

    Prep project update actions

--- src/app/core/actions/project.ts
+++ src/app/core/actions/project.ts
@@ -4,6 +4,10 @@ export const PROJECT_FETCH = '[Project] Fetch'
 export const PROJECT_FETCHED = '[Project] Fetched'
 export const PROJECT_FETCH_ERROR = '[Project] Fetch Error'
 
+export const PROJECT_UPDATE = '[Project] Update'
+export const PROJECT_UPDATED = '[Project] Updated'
+export const PROJECT_UPDATE_ERROR = '[Project] Update Error'
+
 export class ProjectFetch implements Action {
   readonly type = PROJECT_FETCH
   constructor(readonly payload: {id: string}) {}
@@ -20,4 +24,21 @@ export class ProjectFetchError implements Action {
   constructor(readonly payload: any) {}
 }
 
-export type Actions = ProjectFetch|ProjectFetched|ProjectFetchError
+export class ProjectUpdate implements Action {
+  readonly type = PROJECT_UPDATE
+  constructor(readonly payload: {id: string}) {}
+}
+
+export class ProjectUpdated implements Action {
+  readonly type = PROJECT_UPDATED
+  constructor(readonly payload: {id: string}) {}
+}
+
+export class ProjectUpdateError implements Action {
+  readonly type = PROJECT_UPDATED
+  constructor(readonly payload: any) {}
+}
+
+export type Actions =
+  ProjectFetch|ProjectFetched|ProjectFetchError|
+  ProjectUpdate|ProjectUpdated|ProjectUpdateError
