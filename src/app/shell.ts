49b6be3711c175e383d856d54551045cff0fef96
Onur D <contact@dinony.com>
2017-10-13 16:33:09 +0200

    Tmp add routerLinks

--- src/app/shell.ts
+++ src/app/shell.ts
@@ -2,6 +2,11 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'rv-app',
-  template: '<router-outlet></router-outlet>'
+  template: `
+    <nav>
+      <a routerLink="/" routerLinkActive="active">Home</a>
+      <a routerLink="/tmp" routerLinkActive="active">Tmp</a>
+    </nav>
+    <router-outlet></router-outlet>`
 })
 export class AppShellContainer {}
