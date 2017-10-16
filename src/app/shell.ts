db75bc1c47af4958d1668a9d2422aab7eb8b54b7
Onur D <contact@dinony.com>
2017-10-16 17:21:13 +0200

    Comment out tmp nav

--- src/app/shell.ts
+++ src/app/shell.ts
@@ -3,10 +3,10 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'rv-app',
   template: `
-    <nav>
+    <!--<nav>
       <a routerLink="/" routerLinkActive="active">Home</a>
       <a routerLink="/tmp" routerLinkActive="active">Tmp</a>
-    </nav>
+    </nav>-->
     <router-outlet></router-outlet>`
 })
 export class AppShellContainer {}
