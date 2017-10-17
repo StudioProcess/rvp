c58ee8ed7a8edbd8b7816ca84e4c66f5fe97e9be
Onur D <contact@dinony.com>
2017-10-17 12:54:20 +0200

    Mv project btn to separate dir

--- src/app/core/components/projectBtn/projectBtn.component.ts
+++ src/app/core/components/projectBtn/projectBtn.component.ts
@@ -3,15 +3,13 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'rv-project-btn',
   template: `
-    <span data-open="settings-reveal"><i class="ion-ios-folder settings-icon" title="Project Settings"></i>Project</span>
+    <span data-open="settings-reveal">
+      <i class="ion-ios-folder settings-icon" title="Project Settings"></i>Project
+    </span>
     <div class="reveal" id="settings-reveal" data-reveal>
       Test
     </div>
   `,
-  styles: [`
-    .settings-icon {
-      padding-right: 0.5em;
-    }
-  `]
+  styleUrls: ['projectBtn.component.scss']
 })
 export class ProjectBtnComponent {}
