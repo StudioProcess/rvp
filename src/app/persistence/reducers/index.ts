d55879dae1e2616535bea3f092e3d523832d067c
Onur D <contact@dinony.com>
2017-10-23 14:50:27 +0200

    Add flattened annotation selectors

--- src/app/persistence/reducers/index.ts
+++ src/app/persistence/reducers/index.ts
@@ -2,6 +2,8 @@ import {ActionReducerMap, createSelector, createFeatureSelector} from '@ngrx/sto
 
 import * as fromProject from './project'
 
+import {AnnotationColorMap} from '../model'
+
 export interface State {
   project: fromProject.State,
 }
@@ -26,3 +28,29 @@ export const getVideoObjectURL = createSelector(
 export const getTimeline = createSelector(
   getProjectState,
   fromProject.getTimeline)
+
+export const getFlattenedAnnotations = createSelector(getTimeline, timeline => {
+  if(timeline !== null) {
+    const init: AnnotationColorMap[] = []
+    const annotations = timeline.tracks.reduce((accum, track) => {
+      const {color} = track
+      return accum.concat(track.annotations.map(annotation => ({annotation, color})))
+    }, init)
+
+    return annotations
+  } else {
+    return []
+  }
+})
+
+export const getSortedFlattenedAnnotations = createSelector(getFlattenedAnnotations, annotations => {
+  return annotations.sort((a1, a2) => {
+    if(a1.annotation.utc_timestamp < a2.annotation.utc_timestamp) {
+      return -1
+    } else if(a1.annotation.utc_timestamp > a2.annotation.utc_timestamp) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+})
