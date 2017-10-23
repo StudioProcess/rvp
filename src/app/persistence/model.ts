9facf86ebd89feb41b5c875accf997e76aaae1f0
Onur D <contact@dinony.com>
2017-10-23 14:49:59 +0200

    Add model

---
+++ src/app/persistence/model.ts
@@ -0,0 +1,27 @@
+export interface Timeline {
+  duration: number
+  playhead: number
+  zoom: number
+  pan: number
+  tracks: Track[]
+}
+
+export interface Track {
+  color: string
+  fields: {title: string}
+  annotations: Annotation[]
+}
+
+export interface Annotation {
+  utc_timestamp: number
+  duration: number
+  fields: {
+    title: string
+    description: string
+  }
+}
+
+export interface AnnotationColorMap {
+  annotation: Annotation
+  color: string
+}
