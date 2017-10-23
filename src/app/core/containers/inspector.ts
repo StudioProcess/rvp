ad256e4e11eec17e04131af1a6ab8c82e7e5afbf
Onur D <contact@dinony.com>
2017-10-23 14:51:00 +0200

    Use annotation selector in inspector

--- src/app/core/containers/inspector.ts
+++ src/app/core/containers/inspector.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core'
 
+import {Store} from '@ngrx/store'
+
+import * as fromProject from '../../persistence/reducers'
+
 @Component({
   selector: 'rv-inspector',
-  template: `Inspector`
+  template: `
+    <pre>{{annotations|async|json}}</pre>
+  `
 })
 export class InspectorContainer {
+  readonly annotations = this._store.select(fromProject.getSortedFlattenedAnnotations)
 
+  constructor(
+    private readonly _store: Store<fromProject.State>) {}
 }
