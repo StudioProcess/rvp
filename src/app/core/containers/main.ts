46d6eb3dc96e844b4c4ef934e114b27058ee430e
Onur D <contact@dinony.com>
2017-10-24 14:43:37 +0200

    Adapt flex grid column wrapping

--- src/app/core/containers/main.ts
+++ src/app/core/containers/main.ts
@@ -20,13 +20,13 @@ declare var $: any
       <!--<rv-loading *ngIf="_isLoading" [isLoading]="true"></rv-loading>-->
       <div *ngIf="!_isLoading">
         <div class="row">
-          <div class="medium-8 column">
+          <div class="small-12 medium-8 column">
             <rv-player></rv-player>
           </div>
-          <div class="medium-4 column">
+          <div class="small-12 medium-4 column">
             <rv-inspector></rv-inspector>
           </div>
-          <div class="column">
+          <div class="small-12 column">
             <rv-timeline></rv-timeline>
           </div>
         </div>
