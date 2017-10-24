c857e3293a5c825a78f3725a8540e3cd3564edc4
Onur D <contact@dinony.com>
2017-10-24 21:57:42 +0200

    Adapt player config

--- src/app/config/player.ts
+++ src/app/config/player.ts
@@ -5,6 +5,6 @@ export const _DEFAULT_PLAYER_OPTIONS_: PlayerOptions = {
 }
 
 export const _PLAYER_TIMEUPDATE_DEBOUNCE_ = 60
-export const _PLAYER_RESIZE_DEBOUNCE_ = 120
+export const _PLAYER_RESIZE_DEBOUNCE_ = 60
 
-export const _PLAYER_ASPECT_RATIO_ = 16/9;
+export const _PLAYER_ASPECT_RATIO_ = 16/9
