00984fcb182fa99b75ea388e37a3d72e56d0fde9
Onur D <contact@dinony.com>
2017-10-28 18:38:13 +0200

    Restructure player actions

--- src/app/player/actions/index.ts
+++ src/app/player/actions/index.ts
@@ -11,39 +11,42 @@ interface PlayerCreatePayload {
 }
 
 export const PLAYER_CREATE = '[Player] Create'
+export const PLAYER_CREATE_SUCCESS = '[Player] Create Success'
+export const PLAYER_CREATE_ERROR = '[Player] Create Error'
+
+export const PLAYER_DESTROY = '[Player] Destroy'
+export const PLAYER_DESTROY_SUCCESS = '[Player] Destroy Success'
+export const PLAYER_DESTROY_ERROR = '[Player] Destroy Error'
+
+export const PLAYER_SET_CURRENT_TIME = '[Player] Set Current Time'
+
+export const PLAYER_SET_DIMENSIONS = '[Player] Set Dimensions'
+export const PLAYER_SET_DIMENSIONS_SUCCESS = '[Player] Set Dimensions Success'
 
 export class PlayerCreate implements Action {
   readonly type = PLAYER_CREATE
   constructor(readonly payload: PlayerCreatePayload) {}
 }
 
-export const PLAYER_CREATE_SUCCESS = '[Player] Created'
-
 export class PlayerCreateSuccess implements Action {
   readonly type = PLAYER_CREATE_SUCCESS
   // constructor(readonly playload: any) {}
 }
-
-export const PLAYER_CREATE_ERROR = '[Player] Create Error'
 
 export class PlayerCreateError implements Action {
   readonly type = PLAYER_CREATE_ERROR
   constructor(readonly payload: any) {}
 }
 
-export const PLAYER_DESTROY = '[Player] Destroy'
 
 export class PlayerDestroy implements Action {
   readonly type = PLAYER_DESTROY
 }
 
-export const PLAYER_DESTROY_SUCCESS = '[Player] Destroyed'
-
 export class PlayerDestroySuccess implements Action {
   readonly type = PLAYER_DESTROY_SUCCESS
 }
 
-export const PLAYER_DESTROY_ERROR = '[Player] Destroy Error'
 
 export class PlayerDestroyError implements Action {
   readonly type = PLAYER_DESTROY_ERROR
@@ -51,21 +54,16 @@ export class PlayerDestroyError implements Action {
 }
 
 // Player state
-export const PLAYER_SET_CURRENT_TIME = '[Player] Set Current Time'
-
 export class PlayerSetCurrentTime implements Action {
   readonly type = PLAYER_SET_CURRENT_TIME
   constructor(readonly payload: {currentTime:number}) {}
 }
 
 // Player dimensions
-
 interface PlayerDimensionsPayload {
   width: number
   height:number
 }
-
-export const PLAYER_SET_DIMENSIONS = '[Player] Set Dimensions'
 
 export class PlayerSetDimensions implements Action {
   readonly type = PLAYER_SET_DIMENSIONS
