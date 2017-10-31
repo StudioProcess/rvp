2e8152262b73605f863a6effa5ec101e948dc953
Onur D <contact@dinony.com>
2017-10-31 13:13:43 +0100

    Prep track color and title

--- src/app/core/components/timeline/track/track.component.ts
+++ src/app/core/components/timeline/track/track.component.ts
@@ -5,7 +5,16 @@ import {Track} from '../../../../persistence/model'
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'rv-track',
-  template: `{{data|json}}`
+  template: `
+    <div class="row">
+      <div class="column shrink">
+        <div [style.color]="data.color">
+          <i class="ion-record" title="Track Color"></i>
+          <input placeholder="Track Title" title="Track Title">
+        </div>
+      </div>
+    </div>
+  `
 })
 export class TrackComponent {
   @Input() data: Track
