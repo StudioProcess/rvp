9cbf2d48f0a95800fabb28bfee6b68f3cc72c62c
Onur D <contact@dinony.com>
2017-11-17 16:44:56 +0100

    Adapt project selectors

--- src/app/persistence/reducers/index.ts
+++ src/app/persistence/reducers/index.ts
@@ -1,8 +1,10 @@
+import {List, Record} from 'immutable'
+
 import {ActionReducerMap, createSelector, createFeatureSelector} from '@ngrx/store'
 
 import * as fromProject from './project'
 
-import {AnnotationColorMap} from '../model'
+import {AnnotationColorMap, AnnotationColorMapRecordFactory} from '../model'
 
 export interface State {
   project: fromProject.State,
@@ -29,25 +31,36 @@ export const getTimeline = createSelector(
   getProjectState,
   fromProject.getTimeline)
 
+// TODO: use Seq to create flattened?
 export const getFlattenedAnnotations = createSelector(getTimeline, timeline => {
   if(timeline !== null) {
-    const init: AnnotationColorMap[] = []
-    const annotations = timeline.tracks.reduce((accum, track, trackIndex) => {
-      const {color} = track
-      return accum.concat(track.annotations.map(annotation => ({trackIndex, annotation, color})))
+    const init: List<Record<AnnotationColorMap>> = List([])
+    const tracks = timeline.get('tracks', null)
+
+    const flattened = tracks.reduce((accum, track, trackIndex) => {
+      const color = track.get('color', null)
+      const annotations = track.get('annotations', null)
+      return accum.concat(annotations.map(annotation => {
+        return new AnnotationColorMapRecordFactory({
+          trackIndex, color, annotation
+        })
+      }))
     }, init)
 
-    return annotations
+    return flattened
   } else {
-    return []
+    return List([])
   }
 })
 
+const path = ['annotation', 'utc_timestamp']
 export const getSortedFlattenedAnnotations = createSelector(getFlattenedAnnotations, annotations => {
   return annotations.sort((a1, a2) => {
-    if(a1.annotation.utc_timestamp < a2.annotation.utc_timestamp) {
+    const a1Start = a1.getIn(path)
+    const a2Start = a2.getIn(path)
+    if(a1Start < a2Start) {
       return -1
-    } else if(a1.annotation.utc_timestamp > a2.annotation.utc_timestamp) {
+    } else if(a1Start > a2Start) {
       return 1
     } else {
       return 0
