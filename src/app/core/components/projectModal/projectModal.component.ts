bf69317ff762966fcf9c9c997239685c94ee0bbc
Onur D <contact@dinony.com>
2017-11-18 20:20:30 +0100

    Use OnPush in projectModal

--- src/app/core/components/projectModal/projectModal.component.ts
+++ src/app/core/components/projectModal/projectModal.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core'
+import {Component, ChangeDetectionStrategy} from '@angular/core'
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'rv-projectmodal',
   templateUrl: 'projectModal.component.html',
   styleUrls: ['projectModal.component.scss']
