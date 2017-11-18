78ea6dd95e9a03d689a3fa03c005c4d8efda6d13
Onur D <contact@dinony.com>
2017-11-18 19:56:09 +0100

    Add project info in modal

--- src/app/core/components/projectModal/projectModal.component.ts
+++ src/app/core/components/projectModal/projectModal.component.ts
@@ -2,14 +2,7 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: 'rv-projectmodal',
-  template: `
-    <div>
-      <h2><i class="ion-ios-folder icon-title" title="Project Settings"></i>Project</h2>
-      <button class="close-button" data-close aria-label="Close modal" type="button">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  `,
+  templateUrl: 'projectModal.component.html',
   styleUrls: ['projectModal.component.scss']
 })
 export class ProjectModalComponent {}
