370d44f259f4f23b332f2cff4d6427ebabe8703e
Onur D <contact@dinony.com>
2017-11-19 17:56:13 +0100

    Adapt select annotation payload type

--- src/app/core/actions/selection.ts
+++ src/app/core/actions/selection.ts
@@ -2,24 +2,24 @@ import {Action} from '@ngrx/store'
 
 import {Record} from 'immutable'
 
-import {Annotation} from '../../persistence/model'
+import {AnnotationSelection} from '../reducers/selection'
 
 export const SELECTION_SELECT_ANNOTATION = '[Selection] Select Annotation'
 export const SELECTION_DESELECT_ANNOTATION = '[Selection] Deselect Annotation'
 export const SELECTION_RESET_ANNOTATION = '[Selection] Reset Annotation'
 
-export class SelectAnnotationPayload {
-  readonly annotation: Record<Annotation>
+export interface SelectionAnnotationPayload {
+  readonly selection: Record<AnnotationSelection>
 }
 
 export class SelectionSelectAnnotation implements Action {
   readonly type = SELECTION_SELECT_ANNOTATION
-  constructor(readonly payload: SelectAnnotationPayload) {}
+  constructor(readonly payload: SelectionAnnotationPayload) {}
 }
 
 export class SelectionDeselectAnnotation implements Action {
   readonly type = SELECTION_DESELECT_ANNOTATION
-  constructor(readonly payload: SelectAnnotationPayload) {}
+  constructor(readonly payload: SelectionAnnotationPayload) {}
 }
 
 export class SelectionResetAnnotation implements Action {
