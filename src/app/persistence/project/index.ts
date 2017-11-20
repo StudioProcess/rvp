b0b88bdbcc0067f2c9294c4964e12fc1815f8f9b
Onur D <contact@dinony.com>
2017-11-20 21:22:06 +0100

    Adapt loadProject

--- src/app/persistence/project/index.ts
+++ src/app/persistence/project/index.ts
@@ -1,32 +1,27 @@
 import * as JSZip from 'jszip'
 
-import {Observable} from 'rxjs/Observable'
-
-import * as fromProject from '../reducers/project'
-// import {_DEFAULT_FILES_IN_ZIP_} from '../../config'
 import {_ZIP_META_} from '../../config'
 
 import loadBinary from '../binary'
 import {loadZip} from '../zip'
 
-export function extractProject(zip: Observable<JSZip>): Observable<fromProject.State> {
-  return zip.concatMap(z => {
-    const extractPromises = _ZIP_META_.map(meta => {
-      return z.file(meta.file).async(meta.type).then((f:any) => [meta.middleware.postLoad(f), meta])
-    })
-
-    return Observable.fromPromise(Promise.all(extractPromises))
-  }).map(res => {
-    return res.reduce((accum: any, [file, meta]) => {
-      accum[meta.map] = file
-      return accum
-    }, {})
+export async function extractProject(zip: JSZip): Promise<any> {
+  const extractPromises = _ZIP_META_.map(meta => {
+    return zip.file(meta.file)
+      .async(meta.type)
+      .then((f:any) => [meta.middleware.postLoad(f), meta])
   })
+
+  const res = await Promise.all(extractPromises)
+
+  return res.reduce((accum: any, [file, meta]) => {
+    accum[meta.map] = file
+    return accum
+  }, {})
 }
 
-export function loadProject(path: Observable<string>): Observable<fromProject.State> {
-  return extractProject(loadZip(loadBinary(path)))
-}
-
-export function saveProject() {
+export async function loadProject(path: string): Promise<any> {
+  const bin = await loadBinary(path)
+  const zip = await loadZip(bin)
+  return extractProject(zip)
 }
