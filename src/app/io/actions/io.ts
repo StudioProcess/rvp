dda740644e5a680eceb3228ec0f614e20fb88b25
Onur D <contact@dinony.com>
2017-11-20 18:50:05 +0100

    Prep import project action

--- src/app/io/actions/io.ts
+++ src/app/io/actions/io.ts
@@ -1,9 +1,14 @@
 import {Action} from '@ngrx/store'
 
+export const IO_IMPORT_PROJECT = '[IO] Import Project'
 export const IO_EXPORT_PROJECT = '[IO] Export Project'
+
+export class IOImportProject implements Action {
+  readonly type = IO_IMPORT_PROJECT
+}
 
 export class IOExportProject implements Action {
   readonly type = IO_EXPORT_PROJECT
 }
 
-export type Actions = IOExportProject
+export type Actions = IOImportProject|IOExportProject
