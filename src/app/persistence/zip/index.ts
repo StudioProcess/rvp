7f4f92bb8983393b673c1798b5cad67fcd8834ee
Onur D <contact@dinony.com>
2017-11-20 21:22:34 +0100

    Adapt load zip

--- src/app/persistence/zip/index.ts
+++ src/app/persistence/zip/index.ts
@@ -1,15 +1,6 @@
 import * as JSZip from 'jszip'
 
-import {Observable} from 'rxjs/Observable'
-import 'rxjs/add/observable/fromPromise'
-import 'rxjs/add/operator/concatMap'
-
-import {_DEFZIPOTPIONS_} from '../../config'
-
-export function loadZip(data: Observable<ArrayBuffer|string>): Observable<JSZip> {
-  return data.concatMap(d => Observable.fromPromise(JSZip.loadAsync(d)))
+export function loadZip(data: ArrayBuffer|string): Promise<JSZip> {
+  return JSZip.loadAsync(data)
 }
 
-export function genZip(zip: any, options:any=_DEFZIPOTPIONS_) {
-  return zip.generateAsync(options)
-}
