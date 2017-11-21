a0941ef014ebd5e2db2e320e144874ce2d851d1d
Onur D <contact@dinony.com>
2017-11-21 14:35:44 +0100

    Prep import project handler in project modal

--- src/app/core/components/project/projectModal/projectModal.component.ts
+++ src/app/core/components/project/projectModal/projectModal.component.ts
@@ -10,8 +10,15 @@ import {
   styleUrls: ['projectModal.component.scss']
 })
 export class ProjectModalComponent implements OnDestroy {
+  @Output() readonly onImportProject = new EventEmitter()
   @Output() readonly onExportProject = new EventEmitter()
   @Output() readonly onResetProject = new EventEmitter()
+
+  importProject(e: any) {
+    if(e && e.target && e.target.files && e.target.files.length > 0) {
+      this.onImportProject.emit(e.target.files[0])
+    }
+  }
 
   exportProject() {
     this.onExportProject.emit()
