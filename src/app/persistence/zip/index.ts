0c557c99b4ea42358f7194f3d8322f6f5db080ec
Onur D <contact@dinony.com>
2017-11-21 14:35:00 +0100

    Adapt loadZip method

--- src/app/persistence/zip/index.ts
+++ src/app/persistence/zip/index.ts
@@ -1,6 +1,6 @@
 import * as JSZip from 'jszip'
 
-export function loadZip(data: ArrayBuffer|string): Promise<JSZip> {
+export function loadZip(data: ArrayBuffer|Blob|string): Promise<JSZip> {
   return JSZip.loadAsync(data)
 }
 
