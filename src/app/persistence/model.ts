396065c7481eacdbf0f2f90eb86e8bf8cea53d3e
Onur D <contact@dinony.com>
2017-12-13 12:53:20 +0100

    Change ordering in model. Prep discriminated union for project video model

--- src/app/persistence/model.ts
+++ src/app/persistence/model.ts
@@ -1,93 +1,5 @@
 import {List, Record, Set} from 'immutable'
 
-// Record factories
-
-export const ProjectMetaRecordFactory = Record<ProjectMeta>({
-  id: null,
-  timeline: null,
-})
-
-export const ProjectSnapshotRecordFactory = Record<ProjectSnapshot>({
-  timestamp: -1,
-  state: new ProjectMetaRecordFactory()
-})
-
-export const ProjectSnapshotsRecordFactory = Record<ProjectSnapshots>({
-  undo: List<Record<ProjectSnapshot>>(),
-  redo: List<Record<ProjectSnapshot>>()
-})
-
-export const ProjectAnnotationSelectionRecordFactory = Record<ProjectAnnotationSelection>({
-  range: Set(),
-  pick: Set(),
-  selected: null
-})
-
-export const ProjectSelectionRecordFactory = Record<ProjectSelection>({
-  annotation: new ProjectAnnotationSelectionRecordFactory()
-})
-
-export const ProjectRecordFactory = Record<Project>({
-  meta: null,
-  video: null,
-  selection: new ProjectSelectionRecordFactory(),
-  snapshots: new ProjectSnapshotsRecordFactory(),
-  clipboard: Set()
-})
-
-export const TimelineRecordFactory = Record<Timeline>({
-  id: null,
-  duration: -1,
-  // playhead: -1,
-  // zoom: -1,
-  // pan: -1,
-  tracks: List([])
-})
-
-export const TrackFieldsRecordFactory = Record<TrackFields>({
-  title: ''
-})
-
-export const TrackRecordFactory = Record<Track>({
-  id: null,
-  color: '#000',
-  fields: new TrackFieldsRecordFactory(),
-  annotationStacks: List([List([])])
-})
-
-export const AnnotationFieldsRecordFactory = Record<AnnotationFields>({
-  title: '',
-  description: ''
-})
-
-export const AnnotationRecordFactory = Record<Annotation>({
-  id: null,
-  utc_timestamp: -1,
-  duration: -1,
-  fields: new AnnotationFieldsRecordFactory()
-})
-
-export const enum VideoSource {
-  None,
-  Blob,
-  Youtube,
-  Vimeo
-}
-
-export const ProjectVideoRecordFactory = Record<ProjectVideo>({
-  source: VideoSource.None,
-  data: null
-})
-
-export const AnnotationColorMapRecordFactory = Record<AnnotationColorMap>({
-  trackIndex: -1,
-  track: null,
-  annotationStackIndex: -1,
-  annotationIndex: -1,
-  annotation: new AnnotationRecordFactory(),
-  color: '#000'
-})
-
 // Model
 
 export interface Project {
@@ -98,9 +10,33 @@ export interface Project {
   readonly clipboard: Set<Record<AnnotationSelection>>
 }
 
-export interface ProjectVideo {
-  source: VideoSource,
-  data: File|Blob|URL|null
+export const enum VideoType {
+  None,
+  Blob,
+  Url
+}
+
+export const enum VideoUrlSource {
+  Youtube,
+  Vimeo
+}
+
+export type ProjectVideo = BlobVideo|UrlVideo|NullVideo
+
+export interface BlobVideo {
+  kind: VideoType.Blob
+  blob: File|Blob
+}
+
+export class UrlVideo {
+  kind: VideoType.Url
+  source: VideoUrlSource
+  url: URL
+}
+
+// Allow record fallback value
+export class NullVideo {
+  kind: VideoType.None
 }
 
 export interface ProjectMeta {
@@ -192,3 +128,83 @@ export interface AnnotationColorMap {
   readonly annotation: Record<Annotation>
   readonly color: string
 }
+
+// Record factories
+
+export const ProjectMetaRecordFactory = Record<ProjectMeta>({
+  id: null,
+  timeline: null,
+})
+
+export const ProjectSnapshotRecordFactory = Record<ProjectSnapshot>({
+  timestamp: -1,
+  state: new ProjectMetaRecordFactory()
+})
+
+export const ProjectSnapshotsRecordFactory = Record<ProjectSnapshots>({
+  undo: List<Record<ProjectSnapshot>>(),
+  redo: List<Record<ProjectSnapshot>>()
+})
+
+export const ProjectAnnotationSelectionRecordFactory = Record<ProjectAnnotationSelection>({
+  range: Set(),
+  pick: Set(),
+  selected: null
+})
+
+export const ProjectSelectionRecordFactory = Record<ProjectSelection>({
+  annotation: new ProjectAnnotationSelectionRecordFactory()
+})
+
+export const ProjectRecordFactory = Record<Project>({
+  meta: null,
+  video: null,
+  selection: new ProjectSelectionRecordFactory(),
+  snapshots: new ProjectSnapshotsRecordFactory(),
+  clipboard: Set()
+})
+
+export const TimelineRecordFactory = Record<Timeline>({
+  id: null,
+  duration: -1,
+  // playhead: -1,
+  // zoom: -1,
+  // pan: -1,
+  tracks: List([])
+})
+
+export const TrackFieldsRecordFactory = Record<TrackFields>({
+  title: ''
+})
+
+export const TrackRecordFactory = Record<Track>({
+  id: null,
+  color: '#000',
+  fields: new TrackFieldsRecordFactory(),
+  annotationStacks: List([List([])])
+})
+
+export const AnnotationFieldsRecordFactory = Record<AnnotationFields>({
+  title: '',
+  description: ''
+})
+
+export const AnnotationRecordFactory = Record<Annotation>({
+  id: null,
+  utc_timestamp: -1,
+  duration: -1,
+  fields: new AnnotationFieldsRecordFactory()
+})
+
+export const ProjectVideoRecordFactory = Record<ProjectVideo>({
+  kind: VideoType.None
+})
+
+export const AnnotationColorMapRecordFactory = Record<AnnotationColorMap>({
+  trackIndex: -1,
+  track: null,
+  annotationStackIndex: -1,
+  annotationIndex: -1,
+  annotation: new AnnotationRecordFactory(),
+  color: '#000'
+})
