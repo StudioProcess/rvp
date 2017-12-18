66c601f61824d4088309bf8c3091d0f8ab311c70
Onur D <contact@dinony.com>
2017-12-18 13:39:39 +0100

    Add techOrder options to player config

--- src/app/config/player.ts
+++ src/app/config/player.ts
@@ -1,7 +1,8 @@
 import {PlayerOptions} from 'video.js'
 
 export const _DEFAULT_PLAYER_OPTIONS_: PlayerOptions = {
-  controls: true
+  controls: true,
+  techOrder: ['html5', /*'vimeo',*/ 'youtube']
 }
 
 export const _PLAYER_TIMEUPDATE_DEBOUNCE_ = 60
