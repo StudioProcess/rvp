8dc2ad7962f50de9ec2f027c9ab5442880b4d242
Onur D <contact@dinony.com>
2018-01-25 21:13:45 +0100

    Add contrib notes in app footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import {Component} from '@angular/core'
   selector: 'rv-footer',
   template: `
     <strong>
-      Research Video is a project by <a href="http://zhdk.ch">Zurich University of the Arts</a> and <a href="http://process.studio">Studio Process</a>.
+      Research Video is a project by <a href="http://zhdk.ch">Zurich University of the Arts</a> and <a href="http://process.studio">Studio Process</a> + <a href="https://github.com/StudioProcess/rvp#Contributors">contributors</a>.
     </strong>`,
   styleUrls: ['footer.component.scss']
 })
