2817afeab441dec3fd430d7441c7c3df7aae4fc2
Onur D <contact@dinony.com>
2018-05-29 11:07:57 +0200

    Prep version component

---
+++ src/app/core/components/version/version.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+import {environment} from '../../../../environments/environment'
+
+@Component({
+  selector: 'rv-version',
+  template: '<span class="rv-version">rv v{{version}}</span>',
+  styles: [`
+    .rv-version {
+      font-size: 9px;
+      font-weight: bold;
+    }
+  `]
+})
+export class VersionComponent {
+  version=environment.version
+}
