20c4f7c0f7a2c986d06410daff588483c7154439
Onur Doğangönül <contact@dinony.com>
2018-10-05 23:24:15 +0200

    Prep checkbox

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -3,7 +3,9 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'rv-toolbar',
   template: `
-    <div class="toolbar-wrapper">Toolbar :)</div>
+    <div class="toolbar-wrapper">
+      <input type="checkbox"> Current Annotations only
+    </div>
   `,
   styleUrls: ['toolbar.component.scss']
 })
