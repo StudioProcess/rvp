7fba7712a0408002bc929e63f647f0c234c11aeb
Onur Doğangönül <contact@dinony.com>
2018-10-07 00:38:16 +0200

    Add new form controls

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -15,21 +15,43 @@ import {skip} from 'rxjs/operators'
 })
 export class ToolbarComponent implements OnInit, OnChanges {
   @Input('inspectorMode') readonly inspectorModeIn: boolean
+  @Input('search') readonly searchIn: string
+  @Input('applyToTimeline') readonly applyToTimelineIn: boolean
 
   inspectorMode = new FormControl(false)
+  search = new FormControl(null)
+  applyToTimeline = new FormControl(false)
 
   @Output() readonly onInspectorModeChange = this.inspectorMode.valueChanges.pipe(skip(1))
+  @Output() readonly onSearchChange = this.search.valueChanges.pipe(skip(1))
+  @Output() readonly onApplyToTimelineChange = this.applyToTimeline.valueChanges.pipe(skip(1))
 
   ngOnInit() {
     this.inspectorMode.setValue(this.inspectorModeIn)
+    this.search.setValue(this.searchIn)
+    this.applyToTimeline.setValue(this.applyToTimelineIn)
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if(changes.inspectorModeIn !== undefined && !changes.inspectorModeIn.firstChange) {
       const {previousValue, currentValue} = changes.inspectorModeIn
       if(previousValue === undefined || previousValue !== currentValue) {
-        console.log('overwrite formcontrol')
         this.inspectorMode.setValue(currentValue)
+      }
+    }
+
+    if(changes.applyToTimelineIn !== undefined && !changes.applyToTimelineIn.firstChange) {
+      const {previousValue, currentValue} = changes.applyToTimelineIn
+      if(previousValue === undefined || previousValue !== currentValue) {
+        console.log('set')
+        this.applyToTimeline.setValue(currentValue)
+      }
+    }
+
+    if(changes.searchIn !== undefined && !changes.searchIn.firstChange) {
+      const {previousValue, currentValue} = changes.searchIn
+      if(previousValue === undefined || previousValue !== currentValue) {
+        this.search.setValue(currentValue)
       }
     }
   }
