28ad775c01f9d2d88e8d8e274efbdd88d8d59828
Onur Doğangönül <contact@dinony.com>
2018-10-08 00:32:34 +0200

    Adapt form to stop propagation on search input

--- src/app/core/components/toolbar/toolbar.component.ts
+++ src/app/core/components/toolbar/toolbar.component.ts
@@ -1,11 +1,13 @@
 import {
   Component, Input, OnChanges, OnInit,
   Output, ChangeDetectionStrategy,
-  SimpleChanges
+  SimpleChanges, AfterViewInit, ViewChild,
+  ElementRef, EventEmitter
 } from '@angular/core'
-import {FormControl} from '@angular/forms'
+import {FormBuilder, FormGroup} from '@angular/forms'
 
-import {skip, debounceTime} from 'rxjs/operators'
+import {fromEvent, Subscription} from 'rxjs'
+import {skip, debounceTime, pluck} from 'rxjs/operators'
 
 import {_FORM_INPUT_DEBOUNCE_} from '../../../config/form'
 
@@ -15,45 +17,88 @@ import {_FORM_INPUT_DEBOUNCE_} from '../../../config/form'
   styleUrls: ['toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ToolbarComponent implements OnInit, OnChanges {
+export class ToolbarComponent implements OnInit, OnChanges, AfterViewInit {
   @Input('inspectorMode') readonly inspectorModeIn: boolean
   @Input('search') readonly searchIn: string
   @Input('applyToTimeline') readonly applyToTimelineIn: boolean
 
-  inspectorMode = new FormControl(false)
-  search = new FormControl(null)
-  applyToTimeline = new FormControl(false)
+  form: FormGroup|null = null
 
-  @Output() readonly onInspectorModeChange = this.inspectorMode.valueChanges.pipe(skip(1))
-  @Output() readonly onSearchChange = this.search.valueChanges.pipe(skip(1), debounceTime(_FORM_INPUT_DEBOUNCE_))
-  @Output() readonly onApplyToTimelineChange = this.applyToTimeline.valueChanges.pipe(skip(1))
+  @Output() readonly onInspectorModeChange = new EventEmitter<boolean>()
+  @Output() readonly onSearchChange = new EventEmitter<string>()
+  @Output() readonly onApplyToTimelineChange = new EventEmitter<boolean>()
+
+  @ViewChild('search') private readonly _searchRef: ElementRef
+
+  private readonly _subs: Subscription[] = []
+
+  constructor(private readonly _fb: FormBuilder) {}
+
+  private _mapModel() {
+    return {
+      inspectorMode: this.inspectorModeIn,
+      search: this.searchIn,
+      applyToTimeline: this.applyToTimelineIn
+    }
+  }
 
   ngOnInit() {
-    this.inspectorMode.setValue(this.inspectorModeIn)
-    this.search.setValue(this.searchIn)
-    this.applyToTimeline.setValue(this.applyToTimelineIn)
+    this.form = this._fb.group(this._mapModel())
+
+    this._subs.push(
+      this.form.valueChanges
+      .pipe(
+        pluck('inspectorMode'), skip(1))
+      .subscribe((value: boolean) => {
+        this.onInspectorModeChange.emit(value)
+      }))
+
+    this._subs.push(
+      this.form.valueChanges
+      .pipe(
+        pluck('search'), skip(1), debounceTime(_FORM_INPUT_DEBOUNCE_))
+      .subscribe((value: string) => {
+        this.onSearchChange.emit(value)
+      }))
+
+    this._subs.push(
+      this.form.valueChanges
+        .pipe(pluck('applyToTimeline'), skip(1))
+        .subscribe((value: boolean) => {
+          this.onApplyToTimelineChange.emit(value)
+        }))
+  }
+
+  ngAfterViewInit() {
+    this._subs.push(fromEvent(this._searchRef.nativeElement, 'keydown').subscribe((ev: KeyboardEvent) => {
+      ev.stopPropagation()
+    }))
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes.inspectorModeIn !== undefined && !changes.inspectorModeIn.firstChange) {
+    if(this.form !== null && changes.inspectorModeIn !== undefined && !changes.inspectorModeIn.firstChange) {
       const {previousValue, currentValue} = changes.inspectorModeIn
       if(previousValue === undefined || previousValue !== currentValue) {
-        this.inspectorMode.setValue(currentValue)
+        this.form.setValue(this._mapModel())
       }
     }
 
-    if(changes.applyToTimelineIn !== undefined && !changes.applyToTimelineIn.firstChange) {
+    if(this.form !== null && changes.applyToTimelineIn !== undefined && !changes.applyToTimelineIn.firstChange) {
       const {previousValue, currentValue} = changes.applyToTimelineIn
       if(previousValue === undefined || previousValue !== currentValue) {
-        this.applyToTimeline.setValue(currentValue)
+        this.form.setValue(this._mapModel())
       }
     }
 
-    if(changes.searchIn !== undefined && !changes.searchIn.firstChange) {
+    if(this.form !== null && changes.searchIn !== undefined && !changes.searchIn.firstChange) {
       const {previousValue, currentValue} = changes.searchIn
       if(previousValue === undefined || previousValue !== currentValue) {
-        this.search.setValue(currentValue)
+        this.form.setValue(this._mapModel())
       }
     }
+  }
+
+  ngOnDestroy() {
+    this._subs.forEach(sub => sub.unsubscribe())
   }
 }
