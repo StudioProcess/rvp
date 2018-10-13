22ea98c8dd07b9feb792acfb04dff800a9bc2989
Onur Doğangönül <contact@dinony.com>
2018-10-13 07:30:51 +0200

    Adjust logo styling

--- src/app/core/components/logo.component.ts
+++ src/app/core/components/logo.component.ts
@@ -4,14 +4,19 @@ import {Component} from '@angular/core'
   selector: 'rv-logo',
   template: `
     <div class="logo">
-      <img src="/assets/img/rv-logo_1000px.png" width="40px" alt="Research Video Logo" title="Research Video">
+      <img src="/assets/img/rv-logo_1000px.png" alt="Research Video Logo" title="Research Video">
     </div>
   `,
   styles: [`
     .logo {
+      padding: 5px;
       display: inline-block;
       font-size: 15px;
       font-weight: 500;
+    }
+
+    img {
+      width: 25px;
     }
   `]
 })
