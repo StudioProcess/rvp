6088313e74e6ff9fd9e2b3aa2caeaeeb1e004ec6
Onur Doğangönül <contact@dinony.com>
2018-10-13 18:30:11 +0200

    Adapt search config

--- src/app/config/search.ts
+++ src/app/config/search.ts
@@ -3,6 +3,6 @@ import * as Fuse from 'fuse.js'
 // Check http://fusejs.io/ to configure fuzzy search
 export const _FUSE_OPTIONS_: Fuse.FuseOptions = {
   threshold: 0.4,
-  keys: ['annotation.fields.title', 'annotation.fields.description'],
+  keys: ['annotation.fields.description'],
   id: 'annotation.id'
 }
