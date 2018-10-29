f13a108ab191cc09904f786a007338b35c2b6f9b
Onur Doğangönül <contact@dinony.com>
2018-10-29 23:05:46 +0100

    Impl focus annotation reducer (#194)

--- src/app/persistence/reducers/project.ts
+++ src/app/persistence/reducers/project.ts
@@ -493,6 +493,9 @@ export function reducer(state: State = initialState, action: project.Actions): S
       return state
         .setIn(['player', 'width'], action.payload.width)
         .setIn(['player', 'height'], action.payload.height)
+    case project.PROJECT_FOCUS_ANNOTATION:
+      const {annotation} = action.payload
+      return state.setIn(['player', 'currentTime'], annotation.get('utc_timestamp', 0))
     default: {
       return state
     }
