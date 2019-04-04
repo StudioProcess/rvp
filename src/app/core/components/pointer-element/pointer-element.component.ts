bc888f532165007c4673eab9270ac0ffbd36b5b3
Adrian Soluch <adrian.soluch@gmail.com>
2019-04-04 16:39:03 +0200

    cdkDrop drag-drop+

--- src/app/core/components/pointer-element/pointer-element.component.ts
+++ src/app/core/components/pointer-element/pointer-element.component.ts
@@ -1,19 +1,30 @@
 import {
   Component,
   OnInit,
-  HostBinding
+  //Output,
+  //EventEmitter,
+  HostBinding,
+  //HostListener,
   //ViewEncapsulation
 } from '@angular/core';
 
-//import { DragDropModule } from '@angular/cdk/drag-drop';
+import {
+  //CdkDragExit,
+  CdkDragDrop
+  //cdkDragDropped
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'rv-pointer-element',
-  //<div cdkDrag (dropped)="onDrop($event)"
   template: `
-    <div cdkDrag cdkDragBoundary=".video-main-elem"
-      class="annotation-pointer-element annotation-pointer-dot"
+    <div
+      cdkDrag
+      cdkDragBoundary=".video-main-elem"
+      (cdkDragStarted)="dragStarted($event)"
+      (cdkDragReleased)="dragReleased($event)"
+      (cdkDragEnded)="dragEnded($event)"
       [style.left.px]="calculatedX"
+      class="annotation-pointer-element annotation-pointer-dot"
     >
     </div>
   `,
@@ -33,14 +44,40 @@ import {
 })
 export class PointerElementComponent implements OnInit {
 
-  @HostBinding('style.left.px') left: number;
-  @HostBinding('style.top.px') top: number;
+  @HostBinding('style.left.px') left: number
+  @HostBinding('style.top.px') top: number
+
+  //@Output() dropped = new EventEmitter<CdkDragDrop<any>>()
+  //@Output('cdkDragDropped') dropped: EventEmitter<CdkDragDrop<any>>
+  //@HostListener('window:mouseup', ['$event']) mouseUp(event: any) {}
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  /*onDrop(event: CdkDragDrop<string[]>) {
-  }*/
+  dragStarted(event: CdkDragDrop<string[]>) {
+    //console.log('CdkDragStart', event);
+  }
+  dragEnded(event: CdkDragDrop<string[]>) {
+    //console.log('CdkDragEnd', event);
+  }
+  dragReleased(event: CdkDragDrop<string[]>) {
+    console.log('cdkDragReleased', event);
+    let element = event.source.getRootElement();
+    let boundingClientRect = element.getBoundingClientRect();
+    let parentPosition = this.getPosition(element);
+    console.log('x: ' + (boundingClientRect.x - parentPosition.left), 'y: ' + (boundingClientRect.y - parentPosition.top));
+  }
+
+  getPosition(el: any) {
+    let x = 0;
+    let y = 0;
+    while(el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+      x += el.offsetLeft - el.scrollLeft;
+      y += el.offsetTop - el.scrollTop;
+      el = el.offsetParent;
+    }
+    return { top: y, left: x };
+  }
 }
