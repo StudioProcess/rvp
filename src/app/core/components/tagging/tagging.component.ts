78fc4c0e626d3a1f770ea6ca02df94df24d8d603
Adrian Soluch <adrian.soluch@gmail.com>
2019-05-03 18:59:32 +0200

    tagging+

--- src/app/core/components/tagging/tagging.component.ts
+++ src/app/core/components/tagging/tagging.component.ts
@@ -1,7 +1,7 @@
 import {
   Component,
   OnInit,
-  //Input,
+  Input,
   Output,
   EventEmitter,
   //HostBinding,
@@ -50,6 +50,7 @@ import {
   `,*/
   template: `
     <div class="tagging-list" contenteditable="false">
+      {{passed_hashtag_2}}
       <span #tag_editable contenteditable="false" id="tag_editable">{{passed_hashtag}}</span>
       <ul contenteditable="false">
         <li *ngFor="let option of options" [value]="option">
@@ -64,7 +65,8 @@ import {
       position: absolute;
     }
     .tagging-list {
-      margin-top: -10px;
+      margin-top: 1px;
+      margin-left: 1px;
       min-width: 100px;
       padding: 10px;
       background-color: rgba(255, 255, 255, 0.9);
@@ -81,24 +83,36 @@ export class TaggingComponent implements OnInit {
 
   //@ViewChild('inputelem') nameInput: MatInput
   //@ViewChild('tag_editable') private _tagEditable: any
-  //myControl = new FormControl()
   //@Input() @HostBinding('hashTag') bindHashTag = ''
+  //passed_hashtag: string = ''
   options: string[] = ['One', 'Two', 'Three']
-  passed_hashtag: string = ''
 
-  @Output() closeHashTagContainer: EventEmitter<any> = new EventEmitter();
+  @Input() passed_hashtag: string
+
+  @Output() closeHashTagContainer: EventEmitter<any> = new EventEmitter()
 
   constructor(
     private _eref: ElementRef,
+    //private _changeDetector: ChangeDetectorRef
     //private _vcRef: ViewContainerRef,
-    //private _cd: ChangeDetectorRef
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   ngAfterViewInit() {}
 
+  ngOnChanges(change: any) {
+    //console.log("CHANGE", change)
+  }
+
   ngOnDestroy() {}
+
+  /*updatePassedHashtag(hashtag: string) {
+    this._changeDetector.markForCheck()
+    this.passed_hashtag = hashtag
+    this._changeDetector.detectChanges()
+  }*/
 
   onClickOutside(ev: any) {
     if (!this._eref.nativeElement.contains(ev.target)) {
