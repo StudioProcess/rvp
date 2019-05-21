2b5f837f6f6a20162ace8ed84b43cec83ba8eea8
Adrian Soluch <adrian.soluch@gmail.com>
2019-05-21 20:15:55 +0200

    search options : FuseOptions fix

--- src/app/config/search.ts
+++ src/app/config/search.ts
@@ -7,7 +7,7 @@ export const _FUSE_OPTIONS_: Fuse.FuseOptions = {
   id: 'annotation.id'
 }
 
-export const _FUSE_OPTIONS_HASHTAGS_: Fuse.FuseOptions<any> = {
+export const _FUSE_OPTIONS_HASHTAGS_: Fuse.FuseOptions = {
   threshold: 0.2,
   keys: ['annotation.fields.description'],
   id: 'annotation.id'
