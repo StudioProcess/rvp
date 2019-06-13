456cdb95dc609a515a82dfc029c5c4e88e6056f6
Adrian Soluch <adrian.soluch@gmail.com>
2019-06-13 12:55:06 +0200

    fuse.js adjusted

--- src/app/config/search.ts
+++ src/app/config/search.ts
@@ -2,13 +2,20 @@ import * as Fuse from 'fuse.js'
 
 // Check http://fusejs.io/ to configure fuzzy search
 export const _FUSE_OPTIONS_: Fuse.FuseOptions<any> = {
-  threshold: 0.4,
+  threshold: 0.3,
+  tokenize: true,
+  matchAllTokens: true,
+  minMatchCharLength: 2,
   keys: ['annotation.fields.description'],
   id: 'annotation.id'
 }
 
 export const _FUSE_OPTIONS_HASHTAGS_: Fuse.FuseOptions<any> = {
   threshold: 0.2,
+  tokenize: true,
+  matchAllTokens: true,
+  findAllMatches: true,
+  minMatchCharLength: 2,
   keys: ['annotation.fields.description'],
   id: 'annotation.id'
 }
