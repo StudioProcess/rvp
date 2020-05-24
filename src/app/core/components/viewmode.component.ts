ffccdbc23ab3857c797a57d6d5fd8b92a3d1f507
Adrian Soluch <adrian.soluch@gmail.com>
2020-05-24 12:10:19 +0200

    viewmode component+

--- src/app/core/components/viewmode.component.ts
+++ src/app/core/components/viewmode.component.ts
@@ -3,15 +3,24 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'rv-viewmode',
   template: `
-    <div class="viewmode-wrapper">
-      <i class="ion-eye-outline"></i>
+    <div class="viewmode-wrapper" *ngIf="is_shown">
+      <i class="ion-md-eye"></i>
     </div>
   `,
   styles: [`
     .viewmode-wrapper {
-      padding: 5px;
       display: inline-block;
+      position: relative;
+      top: 3px;
+      margin: 0 0 0 20px;
+
+    }
+    .viewmode-wrapper i {
+      font-size: 26px;
     }
   `]
 })
-export class ViewmodeComponent { }
+export class ViewmodeComponent {
+
+  is_shown: boolean = true
+}
